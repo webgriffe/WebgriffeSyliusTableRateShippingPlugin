28c78c5445182a1525884eb15aaaab00ef13f3a4
Manuele Menozzi <manuele.menozzi@gmail.com>
2018-09-22 14:19:21 +0200

    Adds shipping table rates admin menu item

---
+++ src/Menu/AdminMenuListener.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace Webgriffe\SyliusTableRateShippingPlugin\Menu;
+
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+
+final class AdminMenuListener
+{
+    public function addAdminMenuItems(MenuBuilderEvent $event): void
+    {
+        $menu = $event->getMenu();
+        $menu
+            ->getChild('configuration')
+            ->addChild(
+                'webgriffe-sylius-table-rate-plugin-table-rates',
+                ['route' => 'webgriffe_admin_shipping_table_rate_index']
+            )
+            ->setLabel('webgriffe.ui.shipping_table_rates')
+            ->setLabelAttribute('icon', 'pallet')
+        ;
+    }
+}
