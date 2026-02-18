e6dab137051049b2067affbb529e54272f674076
Luca Gallinari <luke.gallinari@gmail.com>
2026-02-18 12:40:29 +0100

    Split migrations
    Version20250923083844 is the effectively "old" version that most already have in their DB, so this can be added manually to the migrations list.
    Version20260218113714 has the only change for the "new" version and this must be executed always.

--- src/Migrations/Version20250923083844.php
+++ src/Migrations/Version20250923083844.php
@@ -15,7 +15,7 @@ final class Version20250923083844 extends AbstractMigration
     #[\Override]
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE webgriffe_sylius_shipping_table_rate (id INT AUTO_INCREMENT NOT NULL, currency_id INT NOT NULL, code VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, weightLimitToRate JSON NOT NULL, UNIQUE INDEX UNIQ_1D5F4E4177153098 (code), INDEX IDX_1D5F4E4138248176 (currency_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE webgriffe_sylius_shipping_table_rate (id INT AUTO_INCREMENT NOT NULL, currency_id INT NOT NULL, code VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, weightLimitToRate JSON NOT NULL, UNIQUE INDEX code_idx (code), INDEX IDX_1D5F4E4138248176 (currency_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE webgriffe_sylius_shipping_table_rate ADD CONSTRAINT FK_1D5F4E4138248176 FOREIGN KEY (currency_id) REFERENCES sylius_currency (id)');
     }
 
---
+++ src/Migrations/Version20260218113714.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webgriffe\SyliusTableRateShippingPlugin\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * @psalm-api
+ */
+final class Version20260218113714 extends AbstractMigration
+{
+    #[\Override]
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE webgriffe_sylius_shipping_table_rate RENAME INDEX code_idx TO UNIQ_1D5F4E4177153098');
+    }
+
+    #[\Override]
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE webgriffe_sylius_shipping_table_rate RENAME INDEX uniq_1d5f4e4177153098 TO code_idx');
+    }
+}
